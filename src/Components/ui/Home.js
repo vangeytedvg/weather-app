f713989b590d8cfb93fed9df32e12a31af5f19e2
vangeytedvg <vangeytedvg@gmail.com>
2020-07-29 18:30:04 +0200

    weather data

--- src/Components/ui/Home.js
+++ src/Components/ui/Home.js
@@ -1,6 +1,7 @@
 // Get weather for today
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import {Progress} from 'reactstrap'
 
 // a33368b2381c90cc3499f94f1d4a2d97 (api key for weatherstack)
 // http://api.weatherstack.com/current?access_key=a33368b2381c90cc3499f94f1d4a2d97query=London
@@ -12,6 +13,7 @@ const Home = () => {
     const [isLoading, setIsLoading] = useState(true)
     const [errorInf, setErrorInfo] = useState({})
     const [data, setData] = useState([])
+    const [weatherData, setWeatherData] = useState({})
 
     useEffect(() => {
         setIsLoading(true)
@@ -25,12 +27,18 @@ const Home = () => {
                 const apiResponse = response.data;
                 console.log(apiResponse)
                 setIsLoading(false)
-                const { current: {cloudcover, feelslike, humidity, temperature}} = apiResponse
-                console.log("RES", cloudcover, feelslike, humidity, temperature)
+                const { current: {cloudcover, feelslike, 
+                    humidity, temperature, 
+                    weather_descriptions,
+                    weather_icons }} = apiResponse
+                console.log("RES", cloudcover, feelslike, humidity, temperature, weather_descriptions, weather_icons)
+                const description = weather_descriptions[0]
+                const icons = weather_icons[0]
+                setWeatherData({cloudcover, feelslike, humidity, temperature, description, icons})
                 setData(apiResponse)
             }).catch(error => {
                 console.log(error);
-            });
+            }); 
 
     }, [])
 
@@ -43,7 +51,11 @@ const Home = () => {
     } else {
         return (
             <div>
-                {setData.data}
+                <p>Cloudcover : {weatherData.cloudcover}</p>
+                <p>Temperature : {weatherData.temperature}</p>
+                <p>Feels like : {weatherData.feelslike}</p>
+                <p>escripttion : {weatherData.description}</p>
+                <img src={weatherData.weather_icons} alt="Weather"/>
             </div>
         )
     }
